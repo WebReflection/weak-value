07d8e255ff8a5e40aa97fd9c09bd59c58f46fe4a
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2021-05-20 18:27:34 +0200

    Fix #1 - Avoid breaking on key changes

--- cjs/index.js
+++ cjs/index.js
@@ -4,14 +4,19 @@ module.exports = class WeakValue extends Map {
     // the Map could have manually removed the key already
     // or it could've changed it by the time the previous
     // reference was GC'd, hence the double check
-    if (this.has(key) && !this.get(key).deref())
-      this.delete(key);
+    if (this.has(key)) {
+      const value = super.get(key);
+      if (value && value.deref())
+        this.delete(key);
+    }
   });
   get(key) {
     const value = super.get(key);
     return value && value.deref();
   }
   set(key, value) {
+    if (this.has(key))
+      this.#registry.unregister(this.get(key));
     this.#registry.register(value, key);
     return super.set(key, new WeakRef(value));
   }
